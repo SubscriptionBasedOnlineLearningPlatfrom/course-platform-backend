97a1826102a471f023feb8caa5434b4e7ef97d65
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-09-20 09:00:16 +0530

    created dashboard models

--- controllers/student/dashboardController.js
+++ controllers/student/dashboardController.js
@@ -1,26 +1,11 @@
+import { da } from "zod/v4/locales";
 import { supabase } from "../../config/supabaseClient.js";
+import { dashboardModel, enrolledCoursesModel } from "../../models/student/dashboardModels.js";
 
 export const dashboardController = async (req, res) => {
     try {
         const studentId = 'd6279018-9f8c-499a-bb78-f40874d2903d' //req.studentId;
-        const { data: dashboardData, error } = await supabase
-            .from('student_dashboard')
-            .select('*')
-            .eq('student_id', studentId)
-            .single();
-        if (error) {
-            return res.status(500).json({ error: error.message });
-        }
-
-        const { data: streakData, error: streakErr } = await supabase.rpc(
-            'get_current_streak',
-            { p_student: studentId }
-        );
-        
-        if (streakErr) {
-            console.log(streakErr);
-            return res.status(500).json({ error: streakErr.message });
-        }
+        const {dashboard: dashboardData, streak: streakData} = dashboardModel(studentId);
 
         return res.status(200).json({
             dashboard:dashboardData,
@@ -35,16 +20,8 @@ export const dashboardController = async (req, res) => {
 export const enrolledCourses = async (req, res) => {
     try {
         const studentId = 'd6279018-9f8c-499a-bb78-f40874d2903d' //req.studentId;   
-
-        const { data: courses, error } = await supabase
-            .from('enrolledcoursedetails')
-            .select('*')
-            .eq('student_id', studentId);   
-
-        if (error) {
-            console.log(error);
-            return res.status(500).json({ error: error.message });
-        }
+        const courses = enrolledCoursesModel(studentId);
+        
         return res.status(200).json({ courses });
 
     }
