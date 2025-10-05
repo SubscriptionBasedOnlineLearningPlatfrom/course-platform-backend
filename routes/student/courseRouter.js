7798aaa07aade7f1991373296048e7014fdb3e30
priya <sivapragasampriya01@gmail.com>
2025-10-05 21:48:26 +0530

    feat: Enhance course fetching functionality with search, ratings, and featured courses

--- routes/student/courseRouter.js
+++ routes/student/courseRouter.js
@@ -1,16 +1,18 @@
 import express from 'express'
 import { checkCourseEnrollment, courseDetails, enrollment, fetchRelatedCourses, postComment, postReply, viewCommentsWithReplies } from '../../controllers/student/courseController.js';
-import { fetchCourses } from "../../controllers/student/getAllCoursesController.js";
+import { fetchCourses, fetchCoursesWithRatings, fetchFeaturedCourses } from "../../controllers/student/getAllCoursesController.js";
 import { studentAuth } from '../../middlewares/authMiddleware.js';
 
 const courseRouter = express.Router();
 
+courseRouter.get("/featured", fetchFeaturedCourses);
+courseRouter.get("/with-ratings", fetchCoursesWithRatings);
+courseRouter.get("/related-courses/:category",fetchRelatedCourses);
+courseRouter.get("/comments-with-replies/:courseId", viewCommentsWithReplies);
+courseRouter.get("/check-enrollment/:courseId",studentAuth, checkCourseEnrollment);
+courseRouter.get("/", fetchCourses);
 courseRouter.get('/:courseId', courseDetails);
 courseRouter.post('/enrollment',studentAuth, enrollment);
-courseRouter.get("/check-enrollment/:courseId",studentAuth, checkCourseEnrollment);
-courseRouter.get("/related-courses/:category",fetchRelatedCourses);
-courseRouter.get("/", fetchCourses);
-courseRouter.get("/comments-with-replies/:courseId", viewCommentsWithReplies);
 courseRouter.post("/create-comment/:courseId",studentAuth, postComment);
 courseRouter.post("/create-reply",studentAuth, postReply);
 
