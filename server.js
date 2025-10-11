fd8d01b4a977681f565359a23329ec0f6599a63c
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-10-11 13:14:38 +0530

    added vercel.json

--- server.js
+++ server.js
@@ -92,6 +92,10 @@ app.use('/student/assignments', studentSubmissionRoutes);
 
 // Routes
 app.use("/auth", authRoutes);
+app.use("/admin/dashboard", adminDashboardRoute);
+app.use("/admin/payments", viewPaymentRoute);
+app.use("/admin/course-management", courseManagementRoutes);
+app.use("/admin/analytics", analyticsRoutes);
 app.use("/admin/auth", adminAuthRoutes);
 app.use("/admin/users", adminUserManagementRoutes);
 
